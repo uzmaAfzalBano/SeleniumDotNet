e4687c6300fd0d1ec91e29e91f26b8a3ea52a3f8
uzmaAfzalBano <uzmaafzalbano@gmail.com>
2022-02-22 23:57:28 +0530

    Add files via upload

---
+++ SeleniumTestProject/SeleniumTestProject/TestCases/LoginTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Threading;
+
+namespace SeleniumTestProject.TestCases
+{
+
+    [Parallelizable]
+    class LoginTest
+    {
+        [Test]
+        public void Test1()
+        {
+
+            var cap = new ChromeOptions().ToCapabilities();
+            IWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4444/"), cap);
+
+            driver.Navigate().GoToUrl("https://demo.onvio.co.uk");
+
+            Thread.Sleep(2000);
+            driver.Close();
+          
+        }
+
+        //[Test]
+        //public void Test2()
+        //{
+        //    var cap = new ChromeOptions().ToCapabilities();
+        //    IWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4444/"), cap);
+        //    driver.Navigate().GoToUrl("https://qed.onvio.co.uk");
+        //    Thread.Sleep(2000);
+        //    driver.Close();
+        //}
+
+
+        //[Test]
+        //public void Test3()
+        //{
+        //    var cap = new ChromeOptions().ToCapabilities();
+        //    IWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4444/"), cap);
+        //    driver.Navigate().GoToUrl("https://www.selenium.dev/");
+        //    Thread.Sleep(2000);
+        //    driver.Close();
+        //}
+    }
+}
---
+++ SeleniumTestProject/SeleniumTestProject/TestCases/Test2.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Threading;
+
+namespace SeleniumTestProject.TestCases
+{
+    [Parallelizable]
+    class Test2
+    {
+        [Test]
+        public void Test21()
+        {
+
+            var cap = new ChromeOptions().ToCapabilities();
+            IWebDriver driver = new RemoteWebDriver(new Uri("http://localhost:4444/"), cap);
+
+            driver.Navigate().GoToUrl("https://qed.onvio.co.uk");
+
+            Thread.Sleep(2000);
+
+        }
+    }
+}
